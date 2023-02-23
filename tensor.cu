41c1e8b3181a05d4d4600882c0823abbe42f26f3
alzoqm <alzoqm@naver.com>
2023-02-23 20:35:38 +0900

    code modify

--- tensor.cu
+++ tensor.cu
@@ -4,101 +4,79 @@ using namespace std;
 
 
 template <typename T>
-Tensor<T>::Tensor(initializer_list<uint16_t> shape) // Constructor for the Tensor class
+Tensor<T>::Tensor(unsigned short *shape, char dim) // Constructor for the Tensor class
 {
-    this->dim = static_cast<int8_t>(shape.size()); // Assign the number of dimensions
+    this->dim = dim; // Assign the number of dimensions
 
-    // Store the shape information in a vector
-    this->tensor_shape = vector<uint16_t>(shape);
+    // Allocate memory for the 'tensor_shape' array and store the shape information
+    this->tensor_shape = new unsigned short[dim];
+    memcpy(this->tensor_shape, shape, sizeof(ushort)*dim);
 
     // Calculate the total size of the tensor by multiplying the shape information
     this->sum_size = 1;
     for(int i=0; i<dim; i++)
     {
-        this->sum_size = this->sum_size * this->tensor_shape[i];
+        this->sum_size = this->sum_size*shape[i];
     }
-
+    
     // Calculate the number of rows and columns in the tensor
     this->m = this->tensor_shape[this->dim-1];
     this->n = this->sum_size / this->m;
-
-    // Allocate memory for the 'value' array and fill it with 0
+    
+    // Allocate memory for the 'value' array and fill it with random values
     this->value = new T[this->sum_size];
-    memset(this->value, 0, sizeof(T)*this->sum_size);
+    for(int i=0; i<this->sum_size; ++i)
+    {
+        value[i] = (((T)rand())/RAND_MAX) + 0.5; // 0~1 range
+    }
 
     // Set the 'is_cuda' member to false (indicating that the tensor is not stored on a GPU)
     this->is_cuda = false;
 }
 
 template <typename T>
-Tensor<T>::Tensor(vector<uint16_t> shape) // Constructor allocation Value to Tensor
+Tensor<T>::Tensor(T *value, unsigned short *shape, char dim) // Constructor allocation Value to Tensor
 {
-    this->dim = static_cast<int8_t>(shape.size()); // Assign the number of dimensions
+    this->dim = dim; // Assign the number of dimensions
 
-    // Store the shape information in a vector
-    this->tensor_shape = vector<uint16_t>(shape);
-
+    // Allocate memory for the 'tensor_shape' array and store the shape information
+    this->tensor_shape = new unsigned short[dim];
+    memcpy(this->tensor_shape, shape, sizeof(ushort)*dim);
+    
     // Calculate the total size of the tensor by multiplying the shape information
     this->sum_size = 1;
     for(int i=0; i<dim; i++)
     {
-        this->sum_size = this->sum_size * this->tensor_shape[i];
+        this->sum_size = this->sum_size*shape[i];
     }
-
+    
     // Calculate the number of rows and columns in the tensor
     this->m = this->tensor_shape[this->dim-1];
     this->n = this->sum_size / this->m;
-
-    // Allocate memory for the 'value' array and fill it with 0
-    this->value = new T[this->sum_size];
-    memset(this->value, 0, sizeof(T)*this->sum_size);
-
-    // Set the 'is_cuda' member to false (indicating that the tensor is not stored on a GPU)
-    this->is_cuda = false;
-}
-
-template <typename T>
-Tensor<T>Tensor(initializer_list<T> value, initializer_list<uint16_t> shape)
-{
-    this->dim = static_cast<int8_t>(shape.size()); // Assign the number of dimensions
-
-    // Store the shape information in a vector
-    this->tensor_shape = vector<uint16_t>(shape);
-
-    // Calculate the total size of the tensor by multiplying the shape information
-    this->sum_size = 1;
-    for(int i=0; i<dim; i++)
-    {
-        this->sum_size = this->sum_size * this->tensor_shape[i];
-    }
-
-    // Calculate the number of rows and columns in the tensor
-    this->m = this->tensor_shape[this->dim-1];
-    this->n = this->sum_size / this->m;
-
+    
     // Allocate memory for the 'value' array and copy the values from the input 'value' argument
     this->value = new T[this->sum_size];
-    copy(value.begin(), value.end(), this->value);
+    memcpy(this->value, value, this->sum_size*sizeof(T));
 
     // Set the 'is_cuda' member to false (indicating that the tensor is not stored on a GPU)
     this->is_cuda = false;
 }
 
 template <typename T>
-Tensor(initializer_list<T> value, initializer_list<uint16_t> shape) // Constructor allocation Value to Tensor
+Tensor<T>::Tensor(T value, unsigned short *shape, char dim) // Constructor allocation Value to Tensor
 {
-    this->dim = static_cast<int8_t>(shape.size()); // Assign the number of dimensions
-
-    // Store the shape information in a vector
-    this->tensor_shape = vector<uint16_t>(shape);
-
+    this->dim = dim; // Assign the number of dimensions
+    // Allocate memory for the 'tensor_shape' array and store the shape information
+    this->tensor_shape = new unsigned short[dim];
+    memcpy(this->tensor_shape, shape, sizeof(ushort)*dim);
+    
     // Calculate the total size of the tensor by multiplying the shape information
     this->sum_size = 1;
     for(int i=0; i<dim; i++)
     {
-        this->sum_size = this->sum_size * this->tensor_shape[i];
+        this->sum_size = this->sum_size*shape[i];
     }
-
+    
     // Calculate the number of rows and columns in the tensor
     this->m = this->tensor_shape[this->dim-1];
     this->n = this->sum_size / this->m;
@@ -121,38 +99,44 @@ Tensor<T>::~Tensor() // Destructor for the Tensor class
     if(this->is_cuda==true) // Check if the tensor is stored on a GPU or in CPU memory
     {
         cudaFree(this->value);
-        this->value=NULL;
     }
     else // else in cpu
     {
         delete this->value;
-        this->value=NULL;
     }
 }
 
 template <typename T>
-vector<uint16_t> Tensor<T>::shape() // Returns the shape of the Tensor2D as an array of unsigned short integers
+unsigned short *Tensor<T>::shape() // Returns the shape of the Tensor2D as an array of unsigned short integers
 {
-    return this->tensor_shape;
+    // Allocate memory for a temporary shape array
+    unsigned short *temp_shape;
+    temp_shape = new unsigned short[this->dim];
+
+    // Copy the shape from 'tensor_shape' to 'temp_shape'
+    memcpy(temp_shape, this->tensor_shape, this->dim);
+
+    // Return the temporary shape array
+    return temp_shape;
 }
 
 template <typename T>
 T *Tensor<T>::return_value() // Returns the value of the Tensor2D as an array of template
 {
-    T* value = new T[this->sum_size]; // Allocate memory for a temporary value array
-    if (this->is_cuda) {
-        T* cpu_value = new T[this->sum_size]; // Allocate memory for a temporary CPU value array
-        cudaError_t status = cudaMemcpy(cpu_value, this->value, this->sum_size * sizeof(T), cudaMemcpyDeviceToHost); // Copy data from device to host
-        if (status != cudaSuccess) {
-            cerr << "Error: " << cudaGetErrorString(status) << endl;
-            exit(EXIT_FAILURE);
-        }
-        copy(cpu_value, cpu_value + this->sum_size, value); // Copy the CPU value array to the temporary value array
-        delete[] cpu_value; // Free the temporary CPU value array
-    } else {
-        copy(this->value, this->value + this->sum_size, value); // Copy the value array to the temporary value array
+    if(this->is_cuda==true) // Check if the tensor is stored on a GPU or in CPU memory
+    {
+        this->cpu(); // If stored on a GPU, transfer it to CPU memory
+        T *value = new T[this->sum_size]; // Allocate memory for a temporary value array
+        memcpy(value, this->value, this->sum_size*sizeof(T)); // Copy the value from 'this->value' to 'value'
+        this->cuda(); // Transfer the tensor back to GPU memory
+        return value; // Return the temporary value array
     }
-    return value; // Return the temporary value array
+    else
+    {
+        T *value = new T[this->sum_size]; // If stored in CPU memory, allocate memory for a temporary value array
+        memcpy(value, this->value, this->sum_size*sizeof(T)); // Copy the value from 'this->value' to 'value'
+        return value; // Return the temporary value array
+    }
 }
 
 template <typename T>
@@ -645,11 +629,14 @@ void Tensor<T>::squeeze(int dim=300) // Later, change default value custom NoneT
     {
         if(this->sum_size==1)
         {
+            delete this->tensor_shape;
             this->dim = 1;
-            this->tensor_shape = {1};
+            this->tensor_shape = new unsigned short[this->dim];
+            this->tensor_shape[0] = 1;
             return;
         }
 
+        unsigned short *temp_tensor_shape;
         int one_dim_cnt=0;
         for(int i=0; i<this->dim; i++)
         {
@@ -663,7 +650,7 @@ void Tensor<T>::squeeze(int dim=300) // Later, change default value custom NoneT
             return;
         }
 
-        vector<uint16_t> temp_tensor_shape(this->dim-one_dim_cnt);
+        temp_tensor_shape = new unsigned short[this->dim-one_dim_cnt];
         int cnt=0;
         for(int i=0; i<this->dim; i++)
         {
@@ -675,8 +662,14 @@ void Tensor<T>::squeeze(int dim=300) // Later, change default value custom NoneT
         }
 
         this->dim = this->dim-one_dim_cnt;
-        this->tensor_shape = move(temp_tensor_shape);
+        delete this->tensor_shape;
+        this->tensor_shape = new unsigned short(this->dim);
 
+        for(int i=0; i<this->dim; i++)
+        {
+            this->tensor_shape[i] = temp_tensor_shape[i];
+        }
+        delete temp_tensor_shape;
         return;
     }
     else
@@ -687,9 +680,9 @@ void Tensor<T>::squeeze(int dim=300) // Later, change default value custom NoneT
         }
         if(this->tensor_shape[dim] != 1)
         {
-            throw invalid_argument("tensor.tensor_shape[argument] is not 1\n");
+            throw std::invalid_argument("tensor.tensor_shape[argument] is not 1");
         }
-        vector<uint16_t> temp_tensor_shape(this->dim-1);
+        unsigned short *temp_tensor_shape = new unsigned short[this->dim-1];
         int cnt = 0;
         for(int i=0; i<this->dim; i++)
         {
@@ -700,8 +693,14 @@ void Tensor<T>::squeeze(int dim=300) // Later, change default value custom NoneT
             temp_tensor_shape[cnt] = this->tensor_shape[i];
             cnt += 1;
         }
+        delete this->tensor_shape;
         this->dim = this->dim-1;
-        this->tensor_shape = move(temp_tensor_shape);
+        this->tensor_shape = new unsigned short[this->dim];
+        for(int i=0; i<this->dim; i++)
+        {
+            this->tensor_shape[i] = temp_tensor_shape[i];
+        }
+        delete temp_tensor_shape;
         return;
     }
 }
@@ -715,10 +714,10 @@ void Tensor<T>::unsqueeze(int dim)
     }
     if(dim > this->dim+1)
     {
-        throw invalid_argument("argument > this->dim+1\n");
+        throw std::invalid_argument("argument > this->dim+1");
     }
+    unsigned short *temp_tensor_shape = new unsigned short[this->dim+1];
 
-    vector<uint16_t> temp_tensor_shape(this->dim + 1);
     int cnt = 0;
     for(int i=0; i<this->dim+1; i++)
     {
@@ -732,64 +731,58 @@ void Tensor<T>::unsqueeze(int dim)
             cnt+=1;
         }
     }
+    delete this->tensor_shape;
     this->dim += 1;
-    this->tensor_shape = move(temp_tensor_shape);
+    this->tensor_shape = new unsigned short[this->dim];
+    memcpy(this->tensor_shape, temp_tensor_shape, this->dim * sizeof(unsigned short));
+    delete temp_tensor_shape;
+    return;
 }
 
 template <typename T>
-void Tensor<T>::reshape(initializer_list<int16_t> reshape_array)
+void Tensor<T>::reshape(short *reshape_array, int dim)
 {
-    int temp_reshape_sum = 1;
-    uint32_t reshape_sum = 1;
-    int8_t m1_check = 0; // -1 check
-    int8_t m1_index = -1;
-    int dim = reshape_array.size();
-
-    short* array_ptr = new short[dim];
-    copy(reshape_array.begin(), reshape_array.end(), array_ptr);
-
-    for (int i = 0; i < dim; i++)
+    int temp_reshape_sum=1;
+    unsigned int reshape_sum = 1;
+    char m1_check = 0; // -1 check
+    char m1_index = -1;
+    for(int i=0; i<dim; i++)
     {
-        if (array_ptr[i] == -1)
+        if(reshape_array[i] == -1)
         {
             m1_check += 1;
             m1_index = i;
         }
-        temp_reshape_sum *= array_ptr[i];
+        temp_reshape_sum *= reshape_array[i];
     }
-
-    if (m1_check >= 2)
+    if(m1_check >= 2)
     {
-        throw runtime_error("The value '-1' can only be used once.\n");
+        throw std::runtime_error("The value '-1' can only be used once.\n");
     }
-
-    if (temp_reshape_sum < 0) // using -1
+    if(temp_reshape_sum < 0) // using -1
     {
         reshape_sum = (-temp_reshape_sum);
-
-        if (this->sum_size % reshape_sum != 0)
+        printf("value: %d\n", reshape_sum);
+        if(this->sum_size % reshape_sum!=0)
         {
-            throw runtime_error("The total size of the original tensor and the size of the newly defined shape must be the same.1\n");
+            throw std::runtime_error("The total size of the original tensor and the size of the newly defined shape must be the same.1\n");
         }
-
         short m1_value = this->sum_size / reshape_sum;
-        array_ptr[m1_index] = m1_value;
-        reshape_sum *= array_ptr[m1_index];
+        reshape_array[m1_index] = m1_value;
+        reshape_sum *= reshape_array[m1_index];
     }
     else
     {
         reshape_sum = temp_reshape_sum;
     }
-
-    if (this->sum_size != reshape_sum)
+    
+    if(this->sum_size != reshape_sum)
     {
-        throw runtime_error("The total size of the original tensor and the size of the newly defined shape must be the same.\n");
+        printf("%d %d\n", this->sum_size, reshape_sum);
+        throw std::runtime_error("The total size of the original tensor and the size of the newly defined shape must be the same.\n");
     }
-
+    delete this->tensor_shape;
     this->dim = dim;
-    this->tensor_shape.clear();
-    this->tensor_shape.reserve(dim);
-    copy(array_ptr, array_ptr + dim, back_inserter(this->tensor_shape));
-
-    delete[] array_ptr;
+    this->tensor_shape = new unsigned short[this->dim];
+    memcpy(this->tensor_shape, reshape_array, sizeof(ushort)*dim);
 }
--- tensor.h
+++ tensor.h
@@ -2,50 +2,39 @@
 #define _TENSOR_H_
 
 #include <cuda_runtime.h>
+#include <cstdlib>
 #include <iostream>
-#include <vector>
+#include <cstdio>
+#include <ctime>
+// #include <unistd.h>
 
 using namespace std;
-
-typedef struct BitField{
-    uint8_t a: 1;
-    uint8_t b: 1;
-    uint8_t c: 1;
-    uint8_t d: 1;
-    uint8_t e: 1;
-    uint8_t f: 1;
-    uint8_t g: 1;
-    uint8_t h: 1;
-}bit_field;
 
 template <typename T>
 class Tensor{
 public:
     T *value; // Pointer to the data stored in the tensor
     T *temp_value; // Pointer to a temporary storage for the data in the tensor
-    vector<uint16_t> tensor_shape; // Array of uint16_t values that represent the shape of the tensor
-    uint32_t n; // Number of rows in the tensor
-    uint32_t m; // Number of columns in the tensor
-    int8_t dim; // int8_tacter that represents the number of dimensions of the tensor
-    uint32_t sum_size; // Integer that represents the sum of the size of the tensor
-    bool *mask; // Later, implement the relevant function
+    unsigned short *tensor_shape; // Array of unsigned short values that represent the shape of the tensor
+    unsigned int n; // Number of rows in the tensor
+    unsigned int m; // Number of columns in the tensor
+    char dim; // Character that represents the number of dimensions of the tensor
+    unsigned int sum_size; // Integer that represents the sum of the size of the tensor
+    bool *mask; // bitfiled로 교체 필요
     bool is_cuda; // Boolean flag that indicates whether the tensor is stored on a GPU or not
-    bool requeird_grad;
-    bool is_leaf;
 public:
-    Tensor() {};
-    Tensor(initializer_list<uint16_t> shape); // Constructor for the Tensor class
-    Tensor(initializer_list<T> value, initializer_list<uint16_t> shape); // Constructor allocation Value to Tensor
-    Tensor(T value, initializer_list<uint16_t> shape); // Constructor allocation Value to Tensor
+    Tensor(unsigned short *shape, char dim); // Constructor for the Tensor class
+    Tensor(T *value, unsigned short *shape, char dim); // Constructor allocation Value to Tensor
+    Tensor(T value, unsigned short *shape, char dim); // Constructor allocation Value to Tensor
     ~Tensor(); // Destructor for the Tensor class
-    vector<uint16_t> shape(); // Returns the shape of the Tensor2D as an array of unsigned short integers
+    unsigned short *shape(); // Returns the shape of the Tensor2D as an array of unsigned short integers
     T *return_value(); // Returns the value of the Tensor2D as an array of template
     void print();
     void cuda(); // Copy the tensor data from the CPU memory to the GPU memory.
     void cpu(); // allocate the tensor data from the GPU memory to the CPU memory.
     void squeeze(int dim);
     void unsqueeze(int dim);
-    void reshape(initializer_list<int16_t> reshape_array);
+    void reshape(short *reshape_array, int dim);
 };
 
 #endif
